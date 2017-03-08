a7c144a7cedc52523b94a72fdee6b1d88d29b536
tjandrayana <tjandrayana.setiawan@tokopedia.com>
2017-03-08 23:33:45 +0700

    remove another reverse function

--- scrap-web/scrapper.go
+++ scrap-web/scrapper.go
@@ -176,11 +176,3 @@ func reverseString(input string) string {
 	}
 	return tamp
 }
-func reverseString(input string) string {
-	var rev string = ""
-
-	for i := len(input); i > 0; i-- {
-		rev += string(input[i])
-	}
-	return rev
-}
